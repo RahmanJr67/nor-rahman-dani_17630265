0c33e23e2be36575c8d0b5c2e2145a94e585b42e
RahmanJr67 <norrahmandani31@gmail.com>
2021-03-11 16:02:07 +0800

    commit pertama praktikum Nor Rahman Dani - 17630265

---
+++ profile.php
@@ -0,0 +1,51 @@
+
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/NRD.jpg" alt="Nor Rahman Dani" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM   :</strong></td>
+                        <td align="left" width="50%">17 63 0265  </td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Nor Rahman Dani</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Kotabaru, 31 Mei 1998</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0813 4654 6180</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">norrahmandani31@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/aman.banteng" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/036-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/rahman_jr67" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                           
+                            
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
